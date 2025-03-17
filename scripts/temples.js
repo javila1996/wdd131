87354a5461178d9c89744620592165b43eaed9ea
javila1996 <javila2@byupathway.edu>
2025-03-16 22:39:16 -0600

    temples page

---
+++ scripts/temples.js
@@ -0,0 +1,5 @@
+ // Update the current year
+ document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+ // Update the last modified date
+ document.getElementById("lastmodified").textContent = document.lastModified;
